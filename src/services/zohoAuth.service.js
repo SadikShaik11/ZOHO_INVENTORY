89cb58ac7183f47007ca25ea20af97bd7f848ac1
sadik <sadik.shaik@medzgo.com>
2022-09-01 02:16:20 +0530

    Zoho inventory poc

--- src/services/zohoAuth.service.js
+++ src/services/zohoAuth.service.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const generateToken = async () => {
   const tokens = await axios
     .post(
-      `https://accounts.zoho.in/oauth/v2/token?grant_type=authorization_code&client_id=1000.YU86BAFZSY5ICWERB9TI2ZBX3TEENW&client_secret=bd4fa93cea9e3bee6be1d3474797da301320750c06&code=${process.env.code}`
+      `https://accounts.zoho.in/oauth/v2/token?grant_type=authorization_code&client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&code=${process.env.code}`
     )
     .then((token) => {
       return token;
